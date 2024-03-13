c4ba72f12e2e631a5b7c678e2c2ad9ed9dee6578
Jerdah <idjerop@gmail.com>
2024-03-13 22:22:49 +0000

    update

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -6,7 +6,7 @@
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @next: pointer to the next node
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
