ab7966d9881c78412053443e7561eb8401fb9b48
Jerdah <idjerop@gmail.com>
2024-03-13 18:27:53 +0000

    update

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,13 @@
 
 def fizzbuzz():
     """
-    Print the numbers from 1 to 100, with the following:
+    Prints the numbers from 1 to 100, with the following rules:
     - For multiples of 3, print 'Fizz' instead of the number
     - For multiples of 5, print 'Buzz' instead of the number
     - For multiples of both 3 and 5, print 'FizzBuzz' instead of the number
-    
-    Each element is followed by a space, and the function does not import any modules.
+
+    Each element is followed by a space, and the function does not import
+    any modules.
     """
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
