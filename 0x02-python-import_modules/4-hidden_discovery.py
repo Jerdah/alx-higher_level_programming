cf272a028b667528796d9b8edf5df36e101eb392
Jerdah <idjerop@gmail.com>
2024-03-14 11:32:38 -0700

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-import hidden_4 as hidden
+import hidden_4
 
 
 if __name__ == "__main__":
 
-    for name in dir(hidden):
+    for name in dir(hidden_4):
         if not name.startswith('__'):
             print(name)
--- 0x02-python-import_modules/hidden_4.py
+++
@@ -1,8 +0,0 @@
-def my_secret_santa():
-    pass
-
-def print_hidden():
-    pass
-
-def print_school():
-    pass
