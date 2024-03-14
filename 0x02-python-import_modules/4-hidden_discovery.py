df3101da34aeedd893ef7dd18ad10da3110b77d5
Jerdah <idjerop@gmail.com>
2024-03-14 19:28:18 +0000

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.8
 
-import hidden_4 as hidden
+import hidden_4
 
+def print_names():
+    names = ["my_secret_santa", "print_hidden", "print_school"]
+    for name in names:
+        print(name)
 
 if __name__ == "__main__":
-
-    for name in dir(hidden):
-        if not name.startswith('__'):
-            print(name)
+    print_names()
