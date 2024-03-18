98aaab729a5591cdf82832530324c5ecfb55dbf3
Jerdah <idjerop@gmail.com>
2024-03-18 12:26:47 +0100

    update

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
     """Function does exactly same as provided Python bytecode"""
-
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
