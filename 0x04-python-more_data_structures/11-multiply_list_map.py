5e0631b73fea744aaaae0f68fefc88a1bee61d7e
Jerdah <idjerop@gmail.com>
2024-03-20 14:43:48 +0100

    update

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+def mutiply_list_map(my_ist=[], number=0):
+    return None if list is None else list(map(lambda x: x * number, my_list))
