45d47c8333c0f31230031037c5d4336788a48320
Jerdah <idjerop@gmail.com>
2024-03-20 15:31:21 +0100

    update

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    """Function multiplies elements of a list by a number using map"""
     return list(map(lambda x: x * number, my_list))
