cb8730c8035970fca1251425e52c9666bdfa9797
Jerdah <idjerop@gmail.com>
2024-03-20 16:20:46 +0100

    update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,36 +1,37 @@
 #include <Python.h>
 #include <object.h>
 #include <listobject.h>
+#include <bytesobject.h>
 
 /**
- * print_python_list - Function prints basic info about Python lists.
- * @p: pointer to a Python object
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A pointer to a Python object (expected to be a PyListObject).
  */
 void print_python_list(PyObject *p)
 {
-	long int size = PyList_Size(p);
-	long int alloc = ((PyListObject *)p)->allocated;
-	int i;
+	Py_ssize_t size = PyList_Size(p);
+	int alloc = ((PyListObject *)p)->allocated;
+	Py_ssize_t i;
 
 	printf("[*] Python list info\n");
-	printf("[*] Size of Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", alloc);
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %d\n", alloc);
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+		printf("Element %zd: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 	}
 }
 
 /**
- * print_python_bytes - Function prints basic info about Python bytes objects.
- * @p: pointer to a Python object
+ * print_python_bytes - Prints basic info about Python bytes objects.
+ * @p: A pointer to a Python object (expected to be a PyBytesObject).
  */
 void print_python_bytes(PyObject *p)
 {
 	char *trying_str;
-	long int size;
-	int i;
+	Py_ssize_t size;
+	Py_ssize_t i;
 
 	printf("[.] bytes object info\n");
 
@@ -41,14 +42,15 @@ void print_python_bytes(PyObject *p)
 	}
 
 	size = PyBytes_Size(p);
-	printf("  size: %ld\n", size);
+	printf("  size: %zd\n", size);
 
 	trying_str = (char *)PyBytes_AsString(p);
 	printf("  trying string: %s\n", trying_str);
 
-	printf("  first %ld bytes: ", (size < 10) ? size + 1 : 10);
+	printf("  first %zd bytes:", (size < 10) ? size + 1 : 10);
+
 	for (i = 0; i < ((size < 10) ? size : 10); i++)
-		printf("%02hhx ", (unsigned char)trying_str[i]);
+		printf(" %02hhx", (unsigned char)trying_str[i]);
 
 	printf("\n");
 }
