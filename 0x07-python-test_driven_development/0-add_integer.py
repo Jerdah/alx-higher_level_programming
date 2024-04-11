a05d46458ff592569efdf26713dfb80b43c1243c
Jerdah <idjerop@gmail.com>
2024-04-11 08:33:17 +0200

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,15 +3,12 @@
 def add_integer(a, b=98):
     """
     Add two integers together.
-    
     Args:
         a (int or float): The first integer to add.
         b (int or float, optional): The second integer to add. Defaults to 98.
-    
-    Raises:
+        Raises:
         TypeError: If a or b is not an integer or float.
-    
-    Returns:
+        Returns:
         int: The sum of a and b.
     """
     if not isinstance(a, (int, float)):
